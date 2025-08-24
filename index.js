c0af7a4bebb927521bd680426585c9ced7ac3e66
Umran <mohammedumran.0416@gmail.com>
2025-08-24 15:13:17 +0100

    created container, created boxes based on n, created prompt for resize button, NEED TO CREATE function for generating the boxes and incoperate the prompt fucntion aswell. Next steps is to create the fucntion, then add mousehover and other functionalities

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const div = document.getElementById('container')
+const div = document.getElementById('container');
 const resizeBtn = document.getElementById('resize')
-let number;
-let gridSize = 0;
 
-for(let i = 0; i < 256; i++){
-    const box = document.createElement('div')
-    box.classList.add('box')
-  
-    div.appendChild(box)
-}
+let n = 16;
 
-resizeBtn.addEventListener('click', () =>{
-        number = prompt('Enter a number between 2 and 64')
-
-    if (number > 64 || number < 2){
-        number = prompt('Enter a number between 2 and 64')
+resizeBtn.addEventListener('click', () => {
+     n = prompt('Enter a number between 2 and 64')
+    while(n < 2 || n > 64){
+        alert('Invalid number try again')
+        n = prompt('Enter a number between 2 and 64')
     }
+
 })
 
-gridSize = number * number;
+
+let grid = n * n;
+let widthAndHeight = 600 / n;
 
 
 
+for (let i = 0; i < grid; i++){
+    const box = document.createElement('div');
+    box.classList.add('box');
 
-//need to add mouse hover
-//need to add a button to change the grid board to different numbers
+    box.style.width = `${widthAndHeight}px`
+    box.style.height = `${widthAndHeight}px`
+    //box.style.background = 'red'
+    box.style.boxSizing = 'border-box'
+
+    div.appendChild(box)
+    
+    
+}
+
